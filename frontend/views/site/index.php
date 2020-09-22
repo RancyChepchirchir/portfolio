1f5a4107f49a57b65ee029f23729f5d6c7c63fa2
RancyChepchirchir <kosgeyrancy2@gmail.com>
2020-09-22 19:06:51 +0300

    First Commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,8 @@
+<?php
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+$this->title = 'Rancy Portfolio';
+?>
+      <h1>Rancy Chepchirchir</h1>
+      <h2>I'm a professional Web Developer from Nairobi Kenya</h2>
+      <a href="<?= Url::to(['site/about'])?>" class="btn-about">About Me</a>
