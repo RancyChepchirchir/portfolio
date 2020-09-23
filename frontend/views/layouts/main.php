6fdf9b60ec4c9206021aae406c955532ccc2f818
RancyChepchirchir <kosgeyrancy2@gmail.com>
2020-09-23 08:47:54 +0300

    Second Commit

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -22,8 +22,8 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <meta content="James Mwangi portfolio" name="descriptison">
-    <meta content="James, Mwangi, php" name="keywords">
+    <meta content="My portfolio" name="descriptison">
+    <meta content="Rancy, Chepchirchir, php" name="keywords">
     
       <!-- Favicons -->
   	<link href="assets/img/favicon.png" rel="icon">
@@ -50,18 +50,18 @@ AppAsset::register($this);
         <ul>
           <li class="active"><a href="<?= Url::to(['site/index']) ?>">Home</a></li>
           <li><a href="<?= Url::to(['site/about']) ?>">About</a></li>
-          <li><a href="#">Resume</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Portfolio</a></li>
+          <li><a href="https://rancychep.co.ke/">My Site</a></li>
+          <li><a href="https://www.linkedin.com/in/rancy-chepchirchir-06321341/">Resume</a></li>
+          <li><a href="https://github.com/RancyChepchirchir">Portfolio</a></li>
+          <li><a href="https://wordpress.com/view/rancychepchirchircom.wordpress.com?aff=13200">Creative Writing</a></li>
           <li><a href="<?= Url::to(['site/contact'])?>">Contact</a></li>
         </ul>
       </nav><!-- .nav-menu -->
 
       <div class="header-social-links">
-        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+        <a href="https://twitter.com/rancy17" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="https://www.facebook.com/rancy.kosgey" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="https://www.linkedin.com/in/rancy-chepchirchir-06321341/" class="linkedin"><i class="icofont-linkedin"></i></i></a>
       </div>
 
     </div>
@@ -77,6 +77,40 @@ AppAsset::register($this);
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
+            <div class="row">
+            <div class="col-lg-4">
+                <h2>Publications</h2>
+
+                <p>Effectiveness of commodity futures as opposed to crop insurance in curbing spot price volatility.</p>
+                <p>Currency portfolio optimization with an innovative covariance matrix estimator (QMLE). </p>
+                <p>Weather Shocks and Commodity Spot Prices</p>
+                <p>Credit Scoring Model with AI (With Imbalanced Dataset)</p>
+                
+                <p><a class="btn btn-default" href="https://rancychep.co.ke/"> My Website &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Skills</h2>
+
+                <p>- Languages: Python, R, C, C++, Matlab/Octave    - Cloud: Amazon Web Services, Google, Azure </p>
+                <p>- Databases: PostgreSQL, Hadoop/HDFS, SQL Server </p>
+                <p>- ML frameworks: Tensorflow, Keras, SkLearn, PyTorch, Pandas, Seaborn, PySpark, NLTK, OpenCV </p>
+                <p>- ML concepts: Regressions, SVM, Clustering, Time Series, NLP, CV, CNN, RNN, LSTM, Auto-encoders </p>
+                <p>- Other software: Eviews, SPSS, SAS and Stata </p>
+                
+                <p><a class="btn btn-default" href="https://www.linkedin.com/in/rancy-chepchirchir-06321341/">LinkedIn &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Creative Writing</h2>
+
+                <p>THE INEVITABLE LIGHT.</p>
+                <p>MY ADVICE TO YOU AMORA.</p>
+                <p>AN OPEN LETTER (FROM AMORA).</p>
+                <p>SIGN OF VICTORY OR FAILURE?.</p>
+                <p>AN OPEN LETTER TO MY LIL’ SISSY.</p>
+
+                <p><a class="btn btn-default" href="https://wordpress.com/view/rancychepchirchircom.wordpress.com?aff=13200"> My_Wordpress &raquo;</a></p>
+            </div>
+        </div>
   </section><!-- End Hero -->
   
     <!-- ======= Footer ======= -->
